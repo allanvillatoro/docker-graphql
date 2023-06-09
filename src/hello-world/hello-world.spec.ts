70cda8951b1aab8a18742c99088824583786a0e2
Allan Villatoro <allanvillatoro@unitec.edu>
2023-06-08 20:54:25 -0600

    agregando tests

---
+++ src/hello-world/hello-world.spec.ts
@@ -0,0 +1,5 @@
+describe('TestingHelloWorld',()=>{
+    test('Agregando mi test hello-world',()=>{
+        expect(2).toBe(2);
+    })
+})
---
+++ src/todo/todo.spec.ts
@@ -0,0 +1,5 @@
+describe('TestingToDo',()=>{
+    test('Agregando mi test ToDo',()=>{
+        expect(2).toBe(2);
+    })
+})
